b8662363e747959b05b379bf8df154b8ef189d84
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2025-02-06 18:30:25 +0100

    Add script for generating queries

--- dataset.py
+++ dataset.py
@@ -1,2 +1,37 @@
-class Dataset:
-    pass
+from typing import Literal
+from datasets import load_dataset, concatenate_datasets
+from tqdm import tqdm
+from llm import LLM
+import nltk
+from nltk import pos_tag
+
+nltk.download('averaged_perceptron_tagger_eng')
+
+class Dataset():
+    def __init__(self, llm:LLM, split: Literal["train", "validation"], limit=None) -> None:
+        self.ds = load_dataset("xlangai/spider")[split]
+
+        if limit is not None:
+            self.ds = self.ds.select(range(limit))
+            
+        self.ds = self.ds.to_list()
+
+        for sample in tqdm(self.ds):
+            sample['question_vector'] = llm.get_hidden_representation(sample["question"])
+            sample['masked_question_vector'] = llm.get_hidden_representation(self.__mask_question(sample))
+
+    def __mask_question(self, sample):
+        question_toks = sample["question_toks"]
+        pos_tags = pos_tag(question_toks)
+        masked_question_toks = [
+            '[MASK]' if pos.startswith('NN') or pos.startswith('JJ') else word
+            for word, pos in pos_tags
+        ]
+        masked_question = ' '.join(masked_question_toks)
+        return masked_question
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, index: int) -> dict:
+        return self.ds[index]
--- generate_predictions.py
+++ generate_predictions.py
@@ -4,12 +4,37 @@ from few_shot_text2sql import FewShotText2SQL
 from llm import LLM
 from prompt_organization import DAILOrganization, FullInformationOrganization, SQLOnlyOrganization
 from zero_shot_text2sql import ZeroShotText2SQL
+import re
+import os
+
+######### TODO make as CLI arguments
+validation_dataset_limit_rows = 100
+train_dataset_limit_rows = 100
+output_folder = "results"
+#########
+
+def write_to_file(text, file_path):
+  with open(file_path, 'a') as file:
+    file.write(text + '\n')
+
+def normalize_sql_query(query):
+    normalized_query= query.replace("```sql", "")
+    normalized_query= normalized_query.replace("```", "")
+    normalized_query= normalized_query.replace(" , ", ", ")
+
+    normalized_query = normalized_query.strip()
+    normalized_query = normalized_query.rstrip(';')
+
+    normalized_query = re.sub(r'\s+', ' ', normalized_query.strip()).lower()
+
+    return normalized_query
 
 
 def main():
     llm = LLM()
 
-    dataset = Dataset()
+    validation_dataset = Dataset(llm, split="validation", limit=validation_dataset_limit_rows)
+    train_dataset = Dataset(llm, split="train", limit=train_dataset_limit_rows)
 
     example_selections = {
         'Random': RandomSelection(llm),
@@ -26,19 +51,33 @@ def main():
 
     n_examples = [2, 4, 8]
 
-    configurations =  {
+    sql_generators =  {
         'Zero-shot': ZeroShotText2SQL(llm)
     }
 
     for n in n_examples:
         for es_name, es in example_selections.items():
             for po_name, po in prompt_organizations.items():
-                configurations[f'{es_name}({n}) + {po_name}'] = FewShotText2SQL(
+                sql_generators[f'{es_name}({n}) + {po_name}'] = FewShotText2SQL(
                     llm, 
-                    dataset=dataset, 
+                    dataset=train_dataset, 
                     example_selection=es, 
                     prompt_organization=po, 
                     n_examples=n)
+
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+                
+    for sample in validation_dataset:
+        for name, generator in sql_generators.items():
+            gold_file_path = os.path.join(output_folder, f"gold_{name}.txt")
+            pred_file_path =  os.path.join(output_folder, f"pred_{name}.txt")
+
+            answer = generator.generate_sql(sample)
+            
+            write_to_file(f"{normalize_sql_query(sample['query'])}\t{sample['db_id']}", gold_file_path)
+            write_to_file(normalize_sql_query(answer), pred_file_path)
 
 if __name__ == "__main__":
     main()
