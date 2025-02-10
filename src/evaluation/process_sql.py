bcebda56aee1358ff9557586f28a0bf1d50d540c
Kateryna Tarelkina <kt.tarelkina@gmail.com>
2025-02-10 20:06:24 +0100

    Ignore as

--- src/evaluation/process_sql.py
+++ src/evaluation/process_sql.py
@@ -249,6 +249,8 @@ def parse_col_unit(toks, start_idx, tables_with_alias, schema, default_tables=No
     if toks[idx] == "distinct":
         idx += 1
         isDistinct = True
+    if toks[idx] == "as":  # Quick fix
+        idx += 1
     agg_id = AGG_OPS.index("none")
     idx, col_id = parse_col(toks, idx, tables_with_alias, schema, default_tables)
 
